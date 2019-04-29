f93cafe3dff7421d1a46cf84d9924b8e075cbc08
Jan Meischner <jan.meischner@googlemail.com>
2019-04-29 14:28:50 +0200

    Refactor current directory extraction

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -4,15 +4,18 @@ use std::path::Path;
 pub mod config;
 
 pub fn get_default_config(path: &Path) -> Config {
-    let given_dir = if let Some(dir) = path.components().last() {
-        if let Some(d) = dir.as_os_str().to_str() {
-            d
-        } else {
-            "Your Project"
-        }
-    } else {
-        "Your Project"
+    let cur_dir = match extract_current_directory_name(path) {
+        Some(dir) => dir,
+        None => "Your Project",
     };
 
-    Config::new(String::from(given_dir))
+    Config::new(String::from(cur_dir))
+}
+
+fn extract_current_directory_name(path: &Path) -> Option<&str> {
+    if let Some(dir) = path.file_name() {
+        dir.to_str()
+    } else {
+        None
+    }
 }
--- src/ui/dialog_config.rs
+++ src/ui/dialog_config.rs
@@ -1,4 +1,5 @@
 use dialoguer::Input;
+use std::env;
 use std::io::Result as IOResult;
 use todoco::Config;
 
@@ -6,8 +7,27 @@ use todoco::Config;
 pub fn ask_for_config() -> IOResult<Config> {
     println!("This utility will walk you through creating a todoco.toml file.");
 
-    // Todo: Default value should be directory name
-    let project_name = Input::new().with_prompt("project name").interact()?;
+    let mut project_input = Input::new();
+
+    if let Some(dir) = extract_current_dir_from_env()? {
+        project_input.default(dir);
+    }
+
+    project_input.with_prompt("project name");
+
+    let project_name = project_input.interact()?;
 
     Ok(Config::new(project_name))
+}
+
+fn extract_current_dir_from_env() -> IOResult<Option<String>> {
+    if let Some(dir) = env::current_dir()?.file_name() {
+        if let Some(string) = dir.to_str() {
+            Ok(Some(String::from(string)))
+        } else {
+            Ok(None)
+        }
+    } else {
+        Ok(None)
+    }
 }
