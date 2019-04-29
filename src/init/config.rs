c8a006d13490d8c9316f03429e4f855b5efb89e6
Jan Meischner <jan.meischner@googlemail.com>
2019-04-29 09:58:50 +0200

    Add test for Config::get

--- src/init/config.rs
+++ src/init/config.rs
@@ -17,7 +17,8 @@ impl Config {
 
     pub fn get(path: &Path) -> IOResult<Config> {
         // Todo: get config file name from app.config
-        path.to_path_buf().push("todoco.toml");
+        let mut path = path.to_path_buf();
+        path.push("todoco.toml");
         let file = File::open(path)?;
         let mut buf_reader = BufReader::new(file);
         let mut contents = String::new();
@@ -38,7 +39,8 @@ impl Config {
             toml::to_string_pretty(&self).expect("It was not possible to serialize configuration.");
 
         // Todo: get filename from app.config
-        path.to_path_buf().push("todoco.toml");
+        let mut path = path.to_path_buf();
+        path.push("todoco.toml");
         let mut file = File::create(path)?;
 
         file.write_all(config_text.as_bytes())?;
@@ -58,7 +60,7 @@ mod tests {
     #[test]
     fn write_config_test() {
         let config = Config::new(String::from("bla"));
-        config.write(&Path::new("todoco.toml"));
+        config.write(&Path::new("."));
         let mut content = String::new();
         File::open("todoco.toml")
             .unwrap()
@@ -66,5 +68,12 @@ mod tests {
         let expected = "name = \'bla\'\n";
         assert_eq!(content, expected);
         remove_file("todoco.toml");
+    }
+
+    #[test]
+    fn get_config_from_file() {
+        let config = Config::get(Path::new("env_tests/mod_init")).unwrap();
+        let expected = Config::new(String::from("test"));
+        assert_eq!(config.name, expected.name);
     }
 }
