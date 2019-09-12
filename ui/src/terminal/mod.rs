093db0b4a8f914fd65101b3ded0519cb7486da68
Jan Meischner <jan.meischner@googlemail.com>
2019-09-12 08:52:17 +0200

    Added Todo

--- ui/src/terminal/mod.rs
+++ ui/src/terminal/mod.rs
@@ -1,7 +1,14 @@
 mod footeroption;
 mod listview;
 
+// Struct for configure Options in the ui footer section
 pub use footeroption::FooterOption;
+
+// Todo: Reevaluate if trait is necessary @refactoring(m)
 pub use listview::pageprinter::ItemPrinter;
+
+// Trait to implement a listview against
 pub use listview::searchterm::SearchTerm;
+
+// Struct to start and handle SearchTerms
 pub use listview::termdialog::TermDialog;
