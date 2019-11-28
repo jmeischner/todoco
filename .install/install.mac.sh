8748bf10808a00b1478f9bbd1c05872cae49e877
Jan Meischner <jan.meischner@daparto.com>
2019-11-28 13:14:17 +0100

    Add some installation scripts

---
+++ .install/install.mac.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+url=$(curl -s https://api.github.com/repos/jmeischner/todoco/releases/latest \
+| grep "browser_download_url.*.mac" \
+| cut -d : -f 2,3 \
+| tr -d \")
+
+curl $url -Lo "/usr/local/bin/todoco"
+chmod +x /usr/local/bin/todoco
